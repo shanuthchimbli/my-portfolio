5b10805e2296c3f30a0f03efc1d07dad5cade729
Shanuth Chimbli <46241580+shanuthchimbli@users.noreply.github.com>
2025-12-10 18:13:00 +0000

    p5

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '',
+  base: '/my-portfolio/',
+
 
   server: {
     host: "::",
